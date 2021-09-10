9675d35c6448095439de1f2789b02d3723cb5ac2
Pbnst <89023405+Pbnst@users.noreply.github.com>
2021-09-10 21:17:27 +0200

    Add files via upload

--- phyton.java
+++ phyton.java
@@ -4,136 +4,52 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 public class  phyton {
-public static void main(String[] args)throws FileNotFoundException, IOException{
-	Scanner input = new Scanner(System.in);
+ public static void main(String[] args)throws FileNotFoundException, IOException{
 	String fileName = "città.txt";
-	Scanner scan = new Scanner(new File(fileName));
-	String rline = "";
-	Boolean iniziato = false;
-	Scanner in = new Scanner(System.in);
-	System.out.println("Scegli tra le seguenti città:");
-	System.out.println("New York");
-	System.out.println("Orlando");
-	System.out.println("Los Angeles");
-	System.out.println("Chicago");
-	System.out.println("Washington");
-	System.out.println("Fine per uscire");
-	statistiche times;
-	String città;
-			
-	do {
-	System.out.println("Scegli la città in cui cercare lavoro");
-	città = in.nextLine();
-	 {
-	 switch(città.toUpperCase()) 
-	 {
-	 case "NEW YORK":
-		 do{
-			 String app = scan.nextLine();
-
-			 if(app.contains("NEW YORK"))
-				 iniziato = true;
-			 if(iniziato)
-			 {
-			 if(app.contains("python"))
-			 {
-				 rline += app;
-			 }
-			 else if(app.contains("."))
-				 break;
-			 }
-		 }while(scan.hasNextLine());
-		 
-		 times =  new statistiche(rline);
-		 times.stampaStatistiche();
-		 break;
-	 case "ORLANDO":
-		 do{
-			 String app = scan.nextLine();
-
-			 if(app.contains("ORLANDO"))
-				 iniziato = true;
-			 if(iniziato)
-			 {
-			 if(app.contains("python"))
-			 {
-				 rline += app;
-				 System.out.println(app);
-			 }
-			 else if(app.contains("."))
-				 break;
-			 }
-		 }while(scan.hasNextLine());
-		 
-		 times =  new statistiche(rline);
-		 times.stampaStatistiche();
-		 break;
-	 case "LOS ANGELES":
-		 do{
-			 String app = scan.nextLine();
-
-			 if(app.contains("LOS ANGELES"))
-				 iniziato = true;
-			 if(iniziato)
-			 {
-			 if(app.contains("python"))
-			 {
-				 rline += app;
-			 }
-			 else if(app.contains("."))
-				 break;
-			 }
-		 }while(scan.hasNextLine());
-		 times =  new statistiche(rline);
-		 times.stampaStatistiche();
-		 break;
-	 case "CHICAGO":
-		 do{
-			 String app = scan.nextLine();
-
-			 if(app.contains("CHICAGO"))
-				 iniziato = true;
-			 if(iniziato)
-			 {
-			 if(app.contains("python"))
-			 {
-				 rline += app;
-			 }
-			 else if(app.contains("."))
-				 break;
-			 }
-		 }while(scan.hasNextLine());
-		 
-		 times =  new statistiche(rline);
-		 times.stampaStatistiche();
-		 break;
-	 case "WASHINGTON":
-		 do{
-			 String app = scan.nextLine();
-
-			 if(app.contains("WASHINGTON"))
-				 iniziato = true;
-			 if(iniziato)
-			 {
-			 if(app.contains("python"))
-			 {
-				 rline += app;
-			 }
-			 else if(app.contains("."))
-				 break;
-			 }
-		 }while(scan.hasNextLine());
-		 
-		 times =  new statistiche(rline);
-		 times.stampaStatistiche();
-		 break;
-	 case "FINE":{
-		 break;	 
-	 }
-     default: System.out.println("inserisci una delle città elencate sopra");
-	 }
+    System.out.println("Scegli tra le seguenti città:");
+    System.out.println("New York");
+    System.out.println("Orlando");
+    System.out.println("Los Angeles");
+    System.out.println("Chicago");
+    System.out.println("Washington");
+    System.out.println("Fine per uscire");
+    statistiche times;
+    String città;
+    String appCitta;
+    String tipoLavoro="";
+    do {
+        Scanner scan = new Scanner(new File(fileName));
+        Boolean iniziato = false;
+        String rline = "";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Scegli la citta in cui cercare lavoro");
+        appCitta = input.nextLine();
+        if(!appCitta.equals("fine"))
+        {
+            System.out.println("Scegli il tipo di lavoro da cercare");
+            tipoLavoro = input.nextLine();
+            while (scan.hasNext()) {
+                città = scan.nextLine();
+                if (città.toUpperCase().contains(appCitta.toUpperCase())) {
+                    iniziato = true;
+                }
+                if (iniziato) {
+                    if (città.toUpperCase().contains(tipoLavoro.toUpperCase())) {
+                        rline += città;
+                    } else if (città.contains("."))
+                        break;
+                }
+            }
+                if (!iniziato)
+                    System.out.println("Citta non trovata");
+                else {
+                    times = new statistiche(rline);
+                    times.stampaStatistiche();
+                }
+        }
+        else
+            break;
+    }while (true);
+}
+}
 	
- }
-}while(città!=null);
-
-}}
