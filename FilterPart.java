95a11f804a53ef3d2e94a2f274d5d5da31e55e33
Pbnst <89023405+Pbnst@users.noreply.github.com>
2021-09-30 17:30:47 +0200

    Add files via upload

---
+++ FilterPart.java
@@ -0,0 +1,39 @@
+package com.progetto.progetto.FIlter;
+import com.progetto.progetto.Model.Job;
+import com.progetto.progetto.Utils.Filter;
+import com.progetto.progetto.Utils.FiltroArrayDiStringa;
+
+/** classe che implementa il filtro per verificare se il lavoro sia in di tipo part time
+ * 
+ * @author pietrobonsanto
+ *@see filter
+ *@see FiltroArrayDiStringa
+ */
+public class FilterPart extends FiltroArrayDiStringa implements Filter{
+	
+
+	 /** Costruttore della classe, che richiama il costruttore della super classe
+	 * 
+	 * @param parametri
+	 */
+	public FilterPart(Object parametri) {
+		super(parametri);
+	}
+
+	/**
+	 * Override del metodo filter, dell'interfaccia Filter che restituisce true se
+	 *  è presente la condizione, false altrimenti
+	 * 
+	 * @see Filter
+	 */
+	@Override
+	public boolean filter(Job job) {
+		for(String p : param) {
+		if(job.getEtype().equals(p)) {
+				return true;
+			}
+		}
+	
+		return false;
+	}
+}
